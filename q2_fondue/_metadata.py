efc121992588781e0d9acb9ba7b084d1590c7a77
Michal Ziemski <mziemski@ethz.ch>
2021-04-13 15:13:46 +0200

    MAINT: QIIME 2 plugin base

--- q2_fondue/_metadata.py
+++ q2_fondue/_metadata.py
@@ -1,3 +1,11 @@
+# ----------------------------------------------------------------------------
+# Copyright (c) 2021, QIIME 2 development team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file LICENSE, distributed with this software.
+# ----------------------------------------------------------------------------
+
 import json
 
 import pandas as pd
@@ -46,11 +54,13 @@ class EFetchResult(EutilsResult):
             if k1 in keys_to_keep and f'{k1}_ATTRIBUTES' in v1.keys():
                 try:
                     for attr in v1[f'{k1}_ATTRIBUTES'][f'{k1}_ATTRIBUTE']:
-                        if attr['TAG'] in processed_meta.keys() and attr['TAG'] not in allowed_duplicates:
+                        if attr['TAG'] in processed_meta.keys() and \
+                                attr['TAG'] not in allowed_duplicates:
                             raise DuplicateKeyError
                         processed_meta[attr['TAG']] = attr.get('VALUE')
                 except Exception as e:
-                    print(f'Exception has occurred: {e}. Contents of the metadata was:')
+                    print(f'Exception has occurred: {e}. '
+                          f'Contents of the metadata was:')
                     pprinter = pprint.PrettyPrinter(indent=4, compact=True)
                     pprinter.pprint(attributes_dict)
 
@@ -59,13 +69,16 @@ class EFetchResult(EutilsResult):
     def add_metadata(self, metadata, uids):
         # use json to quickly get rid of OrderedDicts
         self.metadata_raw = json.loads(json.dumps(parsexml(metadata.read())))
-        parsed_results = self.metadata_raw['EXPERIMENT_PACKAGE_SET']['EXPERIMENT_PACKAGE']
+        parsed_results = self.metadata_raw[
+            'EXPERIMENT_PACKAGE_SET']['EXPERIMENT_PACKAGE']
 
         if isinstance(parsed_results, list):
             for i, uid in enumerate(uids):
-                self.metadata[uid] = self._process_single_run(parsed_results[i])
+                self.metadata[uid] = self._process_single_run(
+                    parsed_results[i])
         else:
-            self.metadata[uids[0]] = self._process_single_run(parsed_results)
+            self.metadata[uids[0]] = self._process_single_run(
+                parsed_results)
 
 
 class EFetchAnalyzer(EutilsAnalyzer):
@@ -77,8 +90,11 @@ class EFetchAnalyzer(EutilsAnalyzer):
             self.result = EFetchResult(response, request)
 
     def analyze_error(self, response, request):
-        print(json.dumps({__name__: {'Response': {'dump': request.dump(),
-                                                  'error': response.getvalue()}}}))
+        print(json.dumps({
+            __name__: {
+                'Response': {
+                    'dump': request.dump(),
+                    'error': response.getvalue()}}}))
 
     def analyze_result(self, response, request):
         self.init_result(response, request)
@@ -86,5 +102,6 @@ class EFetchAnalyzer(EutilsAnalyzer):
 
     # cheating a bit here
     def parse(self, raw_response, request):
-        response = self.convert_response(raw_response.read().decode('utf-8'), request)
+        response = self.convert_response(
+            raw_response.read().decode('utf-8'), request)
         self.analyze_result(response, request)
