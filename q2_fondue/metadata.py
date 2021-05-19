6e36bc58231801ce4d533fdc0f4ce90ecd3e50ef
Michal Ziemski <mziemski@ethz.ch>
2021-05-19 16:58:48 +0200

    add accession id validation using a new entrezpy client

--- q2_fondue/metadata.py
+++ q2_fondue/metadata.py
@@ -9,8 +9,9 @@
 from typing import List
 import pandas as pd
 import entrezpy.efetch.efetcher as ef
+import entrezpy.esearch.esearcher as es
 
-from q2_fondue._entrezpy_clients import EFetchAnalyzer
+from q2_fondue._entrezpy_clients import EFetchAnalyzer, ESearchAnalyzer
 
 
 def _efetcher_inquire(
@@ -26,11 +27,30 @@ def _efetcher_inquire(
     return metadata_response.result.to_df()
 
 
+def _validate_esearch_result(esearcher: es.Esearcher, study_ids: List[str]
+                             ) -> bool:
+    esearch_response = esearcher.inquire(
+        {
+            'db': 'sra',
+            'term': " OR ".join(study_ids),
+            'usehistory': False
+        }, analyzer=ESearchAnalyzer(study_ids)
+    )
+
+    return esearch_response.result.validate_result()
+
+
 def get_metadata(
         study_ids: list, email: str, n_jobs: int = 1) -> pd.DataFrame:
+    # validate the ids
+    esearcher = es.Esearcher(
+        'esearcher', email, apikey=None,
+        apikey_var=None, threads=n_jobs, qid=None
+    )
+    _validate_esearch_result(esearcher, study_ids)
+
     efetcher = ef.Efetcher(
         'efetcher', email, apikey=None,
         apikey_var=None, threads=n_jobs, qid=None
     )
-
     return _efetcher_inquire(efetcher, study_ids)
