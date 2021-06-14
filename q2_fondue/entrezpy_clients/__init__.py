7267bbee428b3fd41838ef7d908d33a84fa018c1
Michal Ziemski <mziemski@ethz.ch>
2021-06-14 09:37:23 +0200

    ENH: add get-metadata action (#5)

--- q2_fondue/entrezpy_clients/__init__.py
+++ q2_fondue/entrezpy_clients/__init__.py
@@ -5,11 +5,3 @@
 #
 # The full license is in the file LICENSE, distributed with this software.
 # ----------------------------------------------------------------------------
-
-import importlib
-
-from ._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
-
-importlib.import_module('q2_fondue.types')
