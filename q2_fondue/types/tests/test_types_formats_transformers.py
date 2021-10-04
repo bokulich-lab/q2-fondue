1b5b0472c3215fdfcea7df05dee4b2427d85da70
Michal Ziemski <mziemski@ethz.ch>
2021-10-04 17:33:27 +0200

    ENH: add basic metadata validation

--- q2_fondue/types/tests/test_types_formats_transformers.py
+++ q2_fondue/types/tests/test_types_formats_transformers.py
@@ -10,6 +10,7 @@ import unittest
 
 import pandas as pd
 import qiime2
+from qiime2.plugin import ValidationError
 from qiime2.plugin.testing import TestPluginBase
 
 from q2_fondue.types import SRAMetadata, SRAMetadataDirFmt, SRAMetadataFormat
@@ -19,11 +20,29 @@ class TestFormats(TestPluginBase):
     package = 'q2_fondue.types.tests'
 
     def test_sra_metadata_fmt(self):
-        meta_path = self.get_data_path('sra-metadata-1.tsv')
+        meta_path = self.get_data_path('sra-metadata.tsv')
         format = SRAMetadataFormat(meta_path, mode='r')
         format.validate()
 
-    # TODO: add more tests here when introducing better validation
+    def test_sra_metadata_fmt_missing_columns(self):
+        meta_path = self.get_data_path('sra-metadata-missing-columns.tsv')
+        format = SRAMetadataFormat(meta_path, mode='r')
+        with self.assertRaisesRegexp(
+                ValidationError,
+                'Some required columns are missing from the metadata file: '
+                'Bases, Study ID'
+        ):
+            format.validate()
+
+    def test_sra_metadata_fmt_missing_ids(self):
+        meta_path = self.get_data_path('sra-metadata-missing-ids.tsv')
+        format = SRAMetadataFormat(meta_path, mode='r')
+        with self.assertRaisesRegexp(
+                ValidationError,
+                'Some samples are missing IDs in the following fields: '
+                'BioSample ID, Study ID'
+        ):
+            format.validate()
 
 
 class TestTypes(TestPluginBase):
@@ -42,7 +61,7 @@ class TestTransformers(TestPluginBase):
 
     def setUp(self):
         super().setUp()
-        meta_path = self.get_data_path('sra-metadata-1.tsv')
+        meta_path = self.get_data_path('sra-metadata.tsv')
         self.sra_meta = SRAMetadataFormat(meta_path, mode='r')
         self.sra_meta_df = pd.read_csv(
             meta_path, sep='\t', header=0, index_col=0, dtype='str')
@@ -58,14 +77,14 @@ class TestTransformers(TestPluginBase):
 
     def test_sra_metadata_to_dataframe(self):
         _, obs = self.transform_format(
-            SRAMetadataFormat, pd.DataFrame, 'sra-metadata-1.tsv'
+            SRAMetadataFormat, pd.DataFrame, 'sra-metadata.tsv'
         )
         self.assertIsInstance(obs, pd.DataFrame)
         pd.testing.assert_frame_equal(obs, self.sra_meta_df)
 
     def test_sra_metadata_to_q2_metadata(self):
         _, obs = self.transform_format(
-            SRAMetadataFormat, qiime2.Metadata, 'sra-metadata-1.tsv'
+            SRAMetadataFormat, qiime2.Metadata, 'sra-metadata.tsv'
         )
         exp = qiime2.Metadata(self.sra_meta_df)
         self.assertEqual(obs, exp)
