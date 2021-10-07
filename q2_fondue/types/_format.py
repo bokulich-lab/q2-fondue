80c0c20a26db070ee5c7a606e6938565bb3467b1
Michal Ziemski <mziemski@ethz.ch>
2021-10-07 17:11:25 +0200

    ENH: add basic metadata validation (#19)
    * ENH: add basic metadata validation
    * modify required header fields

--- q2_fondue/types/_format.py
+++ q2_fondue/types/_format.py
@@ -6,13 +6,41 @@
 # The full license is in the file LICENSE, distributed with this software.
 # ----------------------------------------------------------------------------
 
+import pandas as pd
+from qiime2.plugin import ValidationError
 from qiime2.plugin import model
 
 
-# TODO: add proper validation here
 class SRAMetadataFormat(model.TextFileFormat):
-    def _validate(self, n_records=None):
-        pass
+
+    REQUIRED_IDS = [
+        'ID', 'BioSample ID', 'BioProject ID', 'Experiment ID', 'Study ID'
+    ]
+    REQUIRED_HEADER_FIELDS = [
+        'Organism', 'Instrument', 'Platform', 'Bases', 'Bytes', 'Consent',
+        'Library Selection', 'Library Source', 'Library Layout'
+    ]
+    REQUIRED_HEADER_FIELDS.extend(REQUIRED_IDS)
+
+    def _validate(self):
+        df = pd.read_csv(str(self), sep='\t')
+
+        missing_cols = [
+            x for x in self.REQUIRED_HEADER_FIELDS if x not in df.columns]
+        if missing_cols:
+            raise ValidationError(
+                'Some required columns are missing from the metadata file: '
+                f'{", ".join(missing_cols)}.'
+            )
+
+        # some IDs must be present in all samples
+        nans = df.isnull().sum(axis=0)[self.REQUIRED_IDS]
+        missing_ids = nans.where(nans > 0).dropna().index.tolist()
+        if missing_ids:
+            raise ValidationError(
+                'Some samples are missing IDs in the following fields: '
+                f'{", ".join(missing_ids)}.'
+            )
 
     def _validate_(self, level):
         self._validate()
