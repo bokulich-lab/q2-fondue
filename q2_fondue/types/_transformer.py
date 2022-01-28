7ac644da7a204630cdfe6013377533513ae5c421
Michal Ziemski <mziemski@ethz.ch>
2022-01-28 09:23:40 +0100

    simplify transformers

--- q2_fondue/types/_transformer.py
+++ q2_fondue/types/_transformer.py
@@ -15,6 +15,24 @@ from ._format import (
 )
 
 
+def _meta_fmt_to_metadata(ff):
+    with ff.open() as fh:
+        df = pd.read_csv(fh, sep='\t', header=0, index_col=0, dtype='str')
+        return qiime2.Metadata(df)
+
+
+def _meta_fmt_to_series(ff):
+    with ff.open() as fh:
+        s = pd.read_csv(fh, header=0, dtype='str', squeeze=True)
+        return s
+
+
+def _series_to_meta_fmt(data: pd.Series, meta_fmt):
+    with meta_fmt.open() as fh:
+        data.to_csv(fh, sep='\t', header=True, index=False)
+    return meta_fmt
+
+
 @plugin.register_transformer
 def _1(data: pd.DataFrame) -> (SRAMetadataFormat):
     ff = SRAMetadataFormat()
@@ -32,57 +50,39 @@ def _2(ff: SRAMetadataFormat) -> (pd.DataFrame):
 
 @plugin.register_transformer
 def _3(ff: SRAMetadataFormat) -> (qiime2.Metadata):
-    with ff.open() as fh:
-        df = pd.read_csv(fh, sep='\t', header=0, index_col=0, dtype='str')
-        return qiime2.Metadata(df)
+    return _meta_fmt_to_metadata(ff)
 
 
 @plugin.register_transformer
 def _4(data: pd.Series) -> (SRAFailedIDsFormat):
     ff = SRAFailedIDsFormat()
-    with ff.open() as fh:
-        data.to_csv(fh, sep='\t', header=True, index=False)
-    return ff
+    return _series_to_meta_fmt(data, ff)
 
 
 @plugin.register_transformer
 def _5(ff: SRAFailedIDsFormat) -> (pd.Series):
-    with ff.open() as fh:
-        s = pd.read_csv(
-            fh, header=0, dtype='str', squeeze=True
-        )
-        return s
+    return _meta_fmt_to_series(ff)
 
 
 @plugin.register_transformer
 def _6(ff: SRAFailedIDsFormat) -> (qiime2.Metadata):
-    with ff.open() as fh:
-        df = pd.read_csv(fh, header=0, index_col=0, dtype='str')
-        return qiime2.Metadata(df)
+    return _meta_fmt_to_metadata(ff)
 
 
 @plugin.register_transformer
 def _7(data: pd.Series) -> (NCBIAccessionIDsFormat):
     ff = NCBIAccessionIDsFormat()
-    with ff.open() as fh:
-        data.to_csv(fh, sep='\t', header=True, index=False)
-    return ff
+    return _series_to_meta_fmt(data, ff)
 
 
 @plugin.register_transformer
 def _8(ff: NCBIAccessionIDsFormat) -> (pd.Series):
-    with ff.open() as fh:
-        s = pd.read_csv(
-            fh, header=0, dtype='str', squeeze=True
-        )
-        return s
+    return _meta_fmt_to_series(ff)
 
 
 @plugin.register_transformer
 def _9(ff: NCBIAccessionIDsFormat) -> (qiime2.Metadata):
-    with ff.open() as fh:
-        df = pd.read_csv(fh, header=0, index_col=0, dtype='str')
-        return qiime2.Metadata(df)
+    return _meta_fmt_to_metadata(ff)
 
 
 @plugin.register_transformer
